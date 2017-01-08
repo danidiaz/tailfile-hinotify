6f287cd6e14c8292ebb938f8739179f04cc7ae55
daniel <daniel@bogusemailserver.com>
2017-01-09 00:09:16 +0100

    handle pristine state

--- lib/System/IO/TailFile.hs
+++ lib/System/IO/TailFile.hs
@@ -9,25 +9,27 @@ import Control.Concurrent.MVar
 import Control.Monad
 import Control.Exception
 import System.INotify
-import System.IO (withFile,IOMode(ReadMode),hSeek,SeekMode(AbsoluteSeek),hFileSize)
+import System.IO (withFile,IOMode(ReadMode),hSeek,SeekMode(AbsoluteSeek,SeekFromEnd),hFileSize)
 import System.IO.Error (isDoesNotExistError)
 
 tailFile :: FilePath -> (a -> Data.ByteString.ByteString -> IO a) -> IO a -> IO void
 tailFile filepath callback initial = withINotify $ \i -> do
     sem <- newEmptyMVar
+    state <- initial
     _ <- addWatch i [Modify,MoveSelf,DeleteSelf] filepath (\event -> do _ <- tryTakeMVar sem
                                                                         putMVar sem event)
-    state <- initial
     loop sem state
     where
     loop sem =
-        let go a = do ea' <- tryJust (guard . isDoesNotExistError)
-                                     (withFile filepath ReadMode (\h -> sleeper sem h a))
-                      case ea' of 
-                         Left ()  -> do threadDelay 5e5
-                                        go a -- reuse the state
-                         Right a' -> go a'
-        in  go
+        let go pristine a = do ea' <- tryJust (guard . isDoesNotExistError)
+                                              (withFile filepath ReadMode (\h -> do if pristine then hSeek h SeekFromEnd 0
+                                                                                                else return ()
+                                                                                    sleeper sem h a))
+                               case ea' of 
+                                  Left ()  -> do threadDelay 5e5
+                                                 go False a -- reuse the state
+                                  Right a' -> go False a'
+        in  go True
     sleeper sem h =
         let go ms a = do event <- takeMVar sem
                          size' <- hFileSize h 
