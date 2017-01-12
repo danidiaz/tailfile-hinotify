687e7e5251db59f6322f056ca423dc6533014116
daniel <daniel@bogusemailserver.com>
2017-01-12 14:11:20 +0100

    Haddocks.

--- lib/System/IO/TailFile/Streaming.hs
+++ lib/System/IO/TailFile/Streaming.hs
@@ -12,7 +12,6 @@ import qualified System.IO.TailFile.Foldl
 {-| Tail a file with a function that consumes a 'Stream'.
 -}
 tailFile :: FilePath -- ^ 
-         -> (forall t r. (MonadTrans t, MonadIO (t IO)) => Stream (Of Data.ByteString.ByteString) (t IO) r -- ^ Scary type, but any resonably polymorphic (say, over 'MonadIO') function that consumes a 'Stream' can go here.
-                                                        -> t IO (Of void r))
+         -> (forall t r. (MonadTrans t, MonadIO (t IO)) => Stream (Of Data.ByteString.ByteString) (t IO) r -> t IO (Of void r)) -- ^ Scary type, but any resonably polymorphic (say, over 'MonadIO') function that consumes a 'Stream' can go here.
          -> IO void
 tailFile path consumer = System.IO.TailFile.Foldl.tailFile path (evertMIO consumer) 
