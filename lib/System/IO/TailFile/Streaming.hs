fde5206b4707d91822198a62abbf83be6363399f
Ollie Charles <ollie@ocharles.org.uk>
2018-11-06 16:58:16 +0000

    Support latest hinotify

--- lib/System/IO/TailFile/Streaming.hs
+++ lib/System/IO/TailFile/Streaming.hs
@@ -1,18 +1,17 @@
-{-| Tail files in Unix, using types from the @streraming@ package. 
+{-| Tail files in Unix, using types from the @streraming@ package.
 
  -}
 {-# language RankNTypes #-}
 module System.IO.TailFile.Streaming where
 
 import           Data.ByteString (ByteString)
-import qualified Data.ByteString
 import           Streaming
 import           Streaming.Eversion
 import qualified System.IO.TailFile.Foldl
 
 {-| Tail a file with a function that consumes a 'Stream'.
 -}
-tailFile :: ByteString -- ^ System.Posix.ByteString.FilePath.RawFilePath 
+tailFile :: FilePath
          -> (forall t r. (MonadTrans t, MonadIO (t IO)) => Stream (Of Data.ByteString.ByteString) (t IO) r -> t IO (Of void r)) -- ^ Scary type, but any resonably polymorphic (say, over 'MonadIO') function that consumes a 'Stream' can go here.
          -> IO void
-tailFile path consumer = System.IO.TailFile.Foldl.tailFile path (evertMIO consumer) 
+tailFile path consumer = System.IO.TailFile.Foldl.tailFile path (evertMIO consumer)
